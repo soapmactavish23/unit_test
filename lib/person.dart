ceba3fd96aae0cee4737627993a7aa79cc2a6097
soapmactavish23 <ricknogueira1231@gmail.com>
2022-01-29 21:52:19 -0300

    first commit

---
+++ lib/person.dart
@@ -0,0 +1,23 @@
+import 'dart:math' as math;
+
+class Person {
+  final String name;
+  final int age;
+  final double height;
+  final double weigth;
+
+  Person({
+    required this.name,
+    required this.age,
+    required this.height,
+    required this.weigth,
+  });
+
+  double get imc {
+    var result = weigth / math.pow(height, 2);
+    result = result * 100;
+    return result.roundToDouble() / 100;
+  }
+
+  
+}
